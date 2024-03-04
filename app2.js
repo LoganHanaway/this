4582ad7e58e9e33fa16eaa5b6644a6d7bc7b97fc
Logan Hanaway <lthanaway14@gmail.com>
2024-03-04 17:50:44 -0600

    start video 4

--- app2.js
+++ app2.js
@@ -7,4 +7,25 @@ const cat = {
     }
 }
 
-const bluesDance = cat.dance
+// cat.dance('salsa');
+// const bluesDance = cat.dance;
+// bluesDance('salsa');
+
+
+// start video 3
+
+function whatIsThis(){
+    console.log("this =", this)
+}
+
+const myObj = {
+    func: whatIsThis,
+    color: 'purple'
+};
+
+myObj.func()
+
+// 'this' will be set to whatever you have to the left of the dot 
+
+// start video 4
+
